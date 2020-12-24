813c20d22e30f5b1adfd9237380b543165164537
Isaiah Anthony <isaiah.anthony99@gmail.com>
2020-12-24 14:59:39 -0800

    initial api hookup

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-api.openweathermap.org/data/2.5/weather?q={input}&appid={ API key }
+// variables
+let search = document.getElementById('inputPassword2').value
+
+document.getElementById('search').addEventListener('click', function () {
+  event.preventDefault()
+  axios.get(`api.openweathermap.org/data/2.5/weather?q=${search}&appid=7405e1a609d490d0a6b23e2a2070ceea`)
+    .then(res =>{
+      let city = res.name
+      let data = res.data 
+
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=7405e1a609d490d0a6b23e2a2070ceean`)
+      .then(res => {
+        let data = res.data
+        console.log(data)
+        let current = data.current
+        let 
+      })
+    })
+    .catch(err => console.error(err);
+})
+
+
 
 api.openweathermap.org/data/2.5/forecast?q={input}&appid={ API key }
 
